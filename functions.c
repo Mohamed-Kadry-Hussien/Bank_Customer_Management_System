440536d6e1cc4b2ff37a5253fcf3458b91e1bc40
Hamdi Emad <hamdyemadelgohary2004@gmail.com>
2025-06-29 05:52:39 +0300

    Transferring money between customers and finding them

--- functions.c
+++ functions.c
@@ -3,28 +3,46 @@
 
 Customer customers[MAX_CUSTOMERS];
 
-void transfer_money(Customer *customers, int ID1, int ID2, double amount)
+void transfer_money(Customer *customers, int ID1, int ID2, double amount, int customer_count)
 {
-    int index1 = find_customer(customers, ID1, 0, customer_count);
-    int index2 = find_customer(customers, ID2, 0, customer_count);
+    int index1 = find_customer(customers, customer_count, ID1);
+    int index2 = find_customer(customers, customer_count, ID2);
     if (index1 == -1 || index2 == -1)
     {
         printf("Customer not found\n");
         return;
     }
+    if (index1 == index2)
+    {
+        printf("Same customer!\n");
+        return;
+    }
+    if (amount <= 0)
+    {
+        printf("Invalid amount\n");
+        return;
+    }
+    if (customers[index1].balance < amount)
+    {
+        printf("Insufficient balance\n");
+        return;
+    }
     customers[index1].balance -= amount;
     customers[index2].balance += amount;
+    printf("Transfer of %.2f from customer %d to customer %d completed successfully.\n", amount, ID1, ID2);
 }
 
-int find_customer(Customer *customers, int id, int start, int end)
+int find_customer(Customer *customers, int customer_count, int id)
 {
-    if (start > end)
+    if (customer_count <= 0 || customers == NULL)
+    {
+        printf("No customers available.\n");
         return -1;
-    int mid = start + (end - start) / 2;
-    if (id == customers[mid].id)
-        return mid;
-    else if (id < customers[mid].id)
-        return find_customer(customers, id, start, mid - 1);
-    else
-        return find_customer(customers, id, mid + 1, end);
+    }
+    for (int i = 0; i < customer_count; i++)
+    {
+        if (customers[i].id == id)
+            return i;
+    }
+    return -1;
 }
