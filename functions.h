547fe380534bd68a5f123c3d1cca0c6a94918255
MOHAMED KADRY HUSSIEN <143326933+Mohamed-Kadry-Hussien@users.noreply.github.com>
2025-06-30 14:26:31 +0300

    Update functions.h

--- functions.h
+++ functions.h
@@ -3,8 +3,10 @@
 #define FUNCTIONS_H
 
 #include <stdio.h>
+#include <stdbool.h>
 
 #define MAX_CUSTOMERS 100
+#define PHONE_LENGTH 11
 
 typedef struct {
     int id;
@@ -16,14 +18,17 @@ typedef struct {
 extern Customer customers[MAX_CUSTOMERS];
 extern int customer_count;
 
+bool is_valid_name(const char *name);
+bool is_valid_phone(const char *phone);
 void load_data();
-void save_data();
-void create_customer();
-void edit_customer();
-void print_customer_data();
-void delete_customer();
+void save_data(Customer *customers, int *customer_count, char *file_name);
+int find_customer(Customer *customers, int customer_count, int id);
+void create_customer(Customer *customers, int *customer_count);
+void edit_customer(Customer *customers, int *customer_count);
+void print_customer_data(Customer *customers, int *customer_count);
+void delete_customer(Customer *customers, int *customer_count);
 void deposit();
 void withdraw();
-void transfer_money();
+void transfer_money(Customer *customers, int ID1, int ID2, double amount, int customer_count);
 
 #endif
